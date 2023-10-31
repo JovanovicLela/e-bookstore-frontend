2338a940810f8ea880dd0a46b7ffc4aea4c653d2
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-10-31 13:43:00 +0100

    Updated page size option

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -70,4 +70,10 @@ export class ProductListComponent implements OnInit{
     });
 
   }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
